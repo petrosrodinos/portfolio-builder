f1b717aced56f2e1fd47e58f61fdd89733d71617
Petros <petros.rodinos@yahoo.com>
2025-03-31 21:38:08 +0300

    added navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,16 +13,14 @@ const ovo = Ovo({
 });
 
 export const metadata = {
-  title: "Portfolio - GreatStack",
+  title: "Portfolio",
   description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
-      >
+      <body className={`${outfit.className} ${ovo.className}`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,9 @@
-"use client";
-import { useEffect, useState } from "react";
-import About from "../components/About";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Navbar from "../components/Navbar";
-import Services from "../components/Services";
-import Work from "../components/Work";
-import { useTheme } from "next-themes";
+import Navbar from "@/components/NavBar";
 
 export default function Home() {
-  const { theme, setTheme } = useTheme();
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setTheme(isDarkMode ? "light" : "dark");
-  };
-
-  useEffect(() => {
-    setIsDarkMode(theme === "dark");
-  }, [theme]);
-
   return (
     <>
-      <Navbar isDarkMode={isDarkMode} setIsDarkMode={toggleDarkMode} />
-      <Header />
-      <About isDarkMode={isDarkMode} />
-      <Services isDarkMode={isDarkMode} />
-      <Work isDarkMode={isDarkMode} />
-      <Contact isDarkMode={isDarkMode} />
-      <Footer isDarkMode={isDarkMode} />
+      <Navbar />
     </>
   );
 }
