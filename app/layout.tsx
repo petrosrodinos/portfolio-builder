164aa14d90caeb03c2c28b702638a9d227737808
Petros <petros.rodinos@yahoo.com>
2025-04-05 19:00:39 +0300

    fixed colors,fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
-import { Outfit, Ovo } from "next/font/google";
+import { Inter, Manrope } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import QueryProvider from "@/components/providers/QueryProvider";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 
-const outfit = Outfit({
+const inter = Inter({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
 
-const ovo = Ovo({
+const manrope = Manrope({
   subsets: ["latin"],
-  weight: ["400"],
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata = {
@@ -22,7 +22,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={`${outfit.className} ${ovo.className}`}>
+      {/* <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Manrope:wght@200..800&display=swap"
+          rel="stylesheet"
+        />
+      </head> */}
+      <body className={`${inter.className} ${manrope.className}`}>
         <QueryProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
