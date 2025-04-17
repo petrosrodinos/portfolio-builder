b56f6e4f6b69971c53b51c88cf08d32efa4775b4
Petros <petros.rodinos@yahoo.com>
2025-04-17 14:28:45 +0300

    experience view

---
+++ app/console/portfolio/experience/components/experience-form.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExperienceForm = () => {
+  return <div>hohoh</div>;
+};
+
+export default ExperienceForm;
--- app/console/portfolio/experience/page.tsx
+++ app/console/portfolio/experience/page.tsx
@@ -1,7 +1,14 @@
+import ContentSection from "@/components/content-section";
 import React from "react";
+import ExperienceForm from "./components/experience-form";
+import ExperienceView from "./components/experience-view";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <ContentSection title="Experience" desc="Add your experience details here.">
+      <ExperienceView />
+    </ContentSection>
+  );
 };
 
 export default Experience;
