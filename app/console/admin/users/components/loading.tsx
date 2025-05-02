e4f3737e2d1ece4f96611a36903f4a2f28a58b50
Petros <petros.rodinos@yahoo.com>
2025-05-02 19:26:53 +0300

    skeleton

--- app/console/admin/users/components/loading.tsx
+++ app/console/admin/users/components/loading.tsx
@@ -1,5 +1,53 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <Skeleton className="h-8 w-[250px]" />
+        <Skeleton className="h-8 w-[100px]" />
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>
+                <Skeleton className="h-4 w-[100px]" />
+              </TableHead>
+              <TableHead>
+                <Skeleton className="h-4 w-[100px]" />
+              </TableHead>
+              <TableHead>
+                <Skeleton className="h-4 w-[100px]" />
+              </TableHead>
+              <TableHead>
+                <Skeleton className="h-4 w-[100px]" />
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 5 }).map((_, i) => (
+              <TableRow key={i}>
+                <TableCell>
+                  <Skeleton className="h-4 w-[100px]" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-[100px]" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-[100px]" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-[100px]" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
